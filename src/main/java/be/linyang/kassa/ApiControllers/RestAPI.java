bc51ebf9eb6f5a58a55874f7bc6f849ef408bba7
YangNotFound <yang.lin0487@gmail.com>
2017-10-09 10:13:43 +0200

    fix bug on apiController

--- src/main/java/be/linyang/kassa/ApiControllers/RestAPI.java
+++ src/main/java/be/linyang/kassa/ApiControllers/RestAPI.java
@@ -3,22 +3,28 @@ package be.linyang.kassa.ApiControllers;
 
 import be.linyang.kassa.Model.items.Item;
 import be.linyang.kassa.RestoManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController(value = "/api")
+@RestController
 public class RestAPI {
+
+	private static final Logger LOGGER = LogManager.getLogger(RestAPI.class);
 
     @Autowired
     private RestoManager restoManager;
 
-    @GetMapping(value = "/getItems", produces = "application/json")
-    public @ResponseBody
-    List<Item> getAllItems() {
+
+	@CrossOrigin(origins = "*")
+	@GetMapping(value = "/api/getItems")
+    public List<Item> getAllItems() {
+		LOGGER.info("api getAllItems called");
         return restoManager.getItems();
     }
 }
