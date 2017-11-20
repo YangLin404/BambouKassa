ccbe32830d333163752340b27152a1afec493a20
YangNotFound <yang.lin0487@gmail.com>
2017-11-20 10:29:50 +0100

    add sheduled taks voor daily update of restomanager data

--- src/main/java/be/linyang/kassa/Application.java
+++ src/main/java/be/linyang/kassa/Application.java
@@ -2,11 +2,13 @@ package be.linyang.kassa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * application entrypoint
  */
 @SpringBootApplication
+@EnableScheduling
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
---
+++ src/main/java/be/linyang/kassa/ScheduledTasks/DataReloader.java
@@ -0,0 +1,26 @@
+package be.linyang.kassa.ScheduledTasks;
+
+import be.linyang.kassa.RestoManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataReloader {
+    private static final Logger LOGGER = LogManager.getLogger(DataReloader.class);
+
+    @Autowired
+    private RestoManager restoManager;
+
+    @Scheduled(cron = "0 0 12 1/1 * ? *")
+    public void reloadData() {
+        if (restoManager.reloadData()) {
+            LOGGER.info("sheduled task: reload resto manager data succeed");
+        } else {
+            LOGGER.error("sheduled task: reload resto manager data failed");
+        }
+
+    }
+}
